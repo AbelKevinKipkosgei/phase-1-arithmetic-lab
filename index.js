5424c6032db520ec2efb776ef623a46558d09aa8
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-06-27 00:09:18 +0300

    Fixed an erroneous Math.round to Math.ceil

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let num1 = 31;
 let num2 = 2;
 let multiply = num1 * num2;
 
-let random = Math.round(Math.random() * 10);
+let random = Math.ceil(Math.random() * 10);
 let num3 = 24;
 let num4 = 5;
 let mod = num3 % num4;
